6e99745753481adc620862055880c0c39b0e5805
Oswald Calvario <oswald.calvario@gmail.com>
2020-05-02 21:51:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,13 @@
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   array.each { |numbers| new_array << numbers**2 } 
   return new_array
 end
+=======
+  array.each do |numbers|
+   new_array = [numbers**2] 
+  end
+  return new_array
+end
+>>>>>>> 9f7a92d09c2f8cf0d120c6003489473c40a07d0a
